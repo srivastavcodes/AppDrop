4e4be187451eea583c0b16763eea9df894a9caff
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-02-18 22:09:29 +0530

    refactor: idiomatic column name semantics

--- migrations/000004_create_stores_table.up.sql
+++ migrations/000004_create_stores_table.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS stores
 (
-    id        UUID PRIMARY KEY,
-    name      VARCHAR(255)                NOT NULL,
-    slug      VARCHAR(100)                NOT NULL UNIQUE,
-    createdAt TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    updatedAt TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW()
+    id         UUID PRIMARY KEY,
+    name       VARCHAR(255)                NOT NULL,
+    slug       VARCHAR(100)                NOT NULL UNIQUE,
+    created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 ALTER TABLE pages
